35b8b74fa9acb694f0ec6cf33a743c6e827790f8
Bradley J. Tannor <bradleytannor@gmail.com>
2020-05-07 12:05:48 -0700

    Development Complete
    Added subscription_int.rb. Added method declaration tests.

--- lib/subscription_int.rb
+++ lib/subscription_int.rb
@@ -4,8 +4,52 @@
 require_relative "subscription_int/version"
 
 # SubscriptionInt.
+# @abstract
 # @class_description
 #   A Subscription library interface.
 class SubscriptionInt
-end
 
+  # self.subscribable().
+  # @description
+  #   Gets SUBSCRIBABLE.
+  # @return [Set]
+  #   SUBSCRIBABLE's reference.
+  def self.subscribable()
+  end
+
+  # self.publisher(published = nil).
+  # @description
+  #   Predicate. Verifies a published argument is a publishing type instance.
+  # @param published [.]
+  #   Any publishing type instance.
+  # @return [TrueClass, FalseClass]
+  #   True in the case published's class is a publisher. False otherwise.
+  def self.publisher(published = nil)
+  end
+
+  # self.update(subject = nil).
+  # @description
+  #   Updates subscribers.
+  # @param subject [.]
+  #   Any subscribable instance.
+  # @return [NilClass]
+  #   nil.
+  def self.update(subject = nil)
+  end
+
+  # self.kind_subscription(publisher = nil).
+  # @description
+  #   Discerns the appropriate subscription identifier.
+  # @param publisher [.]
+  #   Any subscribable type.
+  # @return [Constant]
+  #   A subscription contact.
+  # @raise [ArgumentError]
+  #   In the case the argument is not subscribable.
+  def self.kind_subscription(publisher = nil)
+  end
+
+  private_class_method :new
+  private_class_method :kind_subscription
+
+end
--- test/subscription_int_test.rb
+++ test/subscription_int_test.rb
@@ -5,6 +5,9 @@ require_relative 'test_helper'
 #   Tests the SubscriptionInt class.
 class SubscriptionIntTest < Minitest::Test
 
+  # Constants.
+  CLASS = SubscriptionInt
+
   # test_conf_doc_f_ex().
   # @description
   #  The .travis.yml, CODE_OF_CONDUCT.md, Gemfile, LICENSE.txt, README.md, 
@@ -31,6 +34,28 @@ class SubscriptionIntTest < Minitest::Test
   # @description
   #   Set fixtures.
   def setup()
+    @pub_c_m  = CLASS.public_methods(false)
+    @priv_c_m = CLASS.private_methods(false)
+  end
+
+  # test_pubcm_dec().
+  # @description
+  #   'Subscription.subscribable()', 'Subscription.publisher(published = nil)',
+  #   and 'Subscription.update(instance = nil)' were declared.
+  def test_pubcm_dec()
+
+    assert_includes(@pub_c_m, :subscribable)
+    assert_includes(@pub_c_m, :publisher)
+    assert_includes(@pub_c_m, :update)
+
+  end
+
+  # test_privcm_dec().
+  # @description
+  #   'Subscription.kind_subscription(subject = nil)' was declared.
+  def test_privcm_dec()
+    assert_includes(@priv_c_m, :new)
+    assert_includes(@priv_c_m, :kind_subscription)
   end
 
   # teardown().
@@ -40,4 +65,3 @@ class SubscriptionIntTest < Minitest::Test
   end
 
 end
-
